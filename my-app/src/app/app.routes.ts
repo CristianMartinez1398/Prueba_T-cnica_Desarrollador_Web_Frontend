e19014f5497e3d06745feab6180530c457aedce9
CristianMartinez1398 <109698231+CristianMartinez1398@users.noreply.github.com>
2024-11-02 14:42:02 -0600

    actualización

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { PostDetailComponent } from './post-detail/post-detail.component'
 import { PaginationComponent } from './shared/pagination/pagination.component'
 
 export const routes: Routes = [
-    { path: '', component: PostsComponent }, // Componente principal
+    { path: 'posts', component: PostsComponent }, // Componente principal
     { path: 'post-detail/:id', component: PostDetailComponent }, // Detalle del post
     
 ];
